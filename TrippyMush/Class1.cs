44c8d015ae181640a8f652235ab62ae06b177c4b
banphe <93543484+banphe@users.noreply.github.com>
2022-09-26 14:37:42 +0700

    added 1 class

---
+++ TrippyMush/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrippyMush
+{
+    internal class Class1
+    {
+    }
+}
